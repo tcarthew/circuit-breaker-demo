ce9d87766f5c0d04fb805aaa17861cb28bc647e0
Tim <tcarthew@gmail.com>
2018-09-25 14:59:02 +0200

    Add missing interface files

---
+++ circuit-breaker-app/interfaces/ICircuitBreakerStateStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace circuit_breaker_app.interfaces
+{
+    public enum CircuitBreakerState
+    {
+        Closed = 1,
+        HalfOpen = 2,
+        Open = 3
+    }
+
+    public interface ICircuitBreakerStateStore
+    {
+        CircuitBreakerState State { get; }
+
+        Exception LastException { get; }
+
+        DateTime? LastChangedDateTime { get; }
+
+        void Trip(Exception ex);
+
+        void Reset();
+
+        void HalfOpen();
+
+        bool IsClosed { get; }
+    }
+}
